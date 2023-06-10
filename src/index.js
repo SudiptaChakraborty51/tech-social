ecf4bfa7cb65ae9b8d5125e617cfa2186c867f23
Sudipta <hisudipta26@gmail.com>
2023-06-10 17:01:50 +0530

    chore: added backend, server files and packages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { makeServer } from "./server";
+
+// Call make Server
+makeServer();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
