b8c8451dbe59800e1992bd1a277a84cf92d2fa01
Sudipta <hisudipta26@gmail.com>
2023-06-11 22:17:44 +0530

    feat: added login route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/landing";
+import Login from "./pages/Login/login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
