3821cb710f20b4392fe53a4f978d94067cf3e6b9
Sudipta <hisudipta26@gmail.com>
2023-06-12 19:51:21 +0530

    added mockman

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import Mockman from "mockman-js";
+
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing/landing";
 import Login from "./pages/Login/login";
@@ -8,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/mockman" element={<Mockman />} />
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
