e581700049e3156d8421e6012685a366b66fca1c
Sudipta <hisudipta26@gmail.com>
2023-06-13 14:13:42 +0530

    feat: added scrollToTop func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import ScrollToTop from "./components/scrollToTop";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <AuthProvider>
         <App />
       </AuthProvider>
