947234207109262e5f7ecb9b41e3f3dad052d4eb
Sudipta <hisudipta26@gmail.com>
2023-06-13 17:54:40 +0530

    added leftSidebar component

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import "./home.css";
 import Navbar from '../../components/Navbar/navbar';
+import LeftSideBar from '../../components/LeftSideBar/leftSideBar';
 
 const Home = () => {
     document.title = "tech-social | Home";
   return (
     <div className='home'>
       <Navbar />
+      <div className='home-content'>
+        <LeftSideBar />
+      </div>
     </div>
   )
 }
