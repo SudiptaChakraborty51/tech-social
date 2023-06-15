2cefa42a2f400ba6fbbeaecbb563f28750f319cf
Sudipta <hisudipta26@gmail.com>
2023-06-15 23:01:27 +0530

    update posts db

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -11,7 +11,8 @@ export const posts = [
     content:
       "Excited to share my first e-commerce website built using ReactJS, mockbee server, vanilla CSS🥳.",
     contentLink: "https://glamour-ecommerce.vercel.app/",
-    mediaURL: "https://res.cloudinary.com/dqlasoiaw/video/upload/v1686771977/tech-social/Glamour_Ecommerce_-_Google_Chrome_2023-06-08_17-22-56_online-video-cutter.com_uaylnt.mp4",
+    mediaURL:
+      "https://res.cloudinary.com/dqlasoiaw/video/upload/v1686771977/tech-social/Glamour_Ecommerce_-_Google_Chrome_2023-06-08_17-22-56_online-video-cutter.com_uaylnt.mp4",
     likes: {
       likeCount: 25,
       likedBy: [],
@@ -24,9 +25,9 @@ export const posts = [
         text: "Awesome work Sudipta 🔥",
         votes: {
           upvotedBy: [],
-          downvotedBy: []
-        }
-      }
+          downvotedBy: [],
+        },
+      },
     ],
     username: "schakraborty",
     createdAt: "2023-05-21T10:55:06+05:30",
@@ -37,7 +38,8 @@ export const posts = [
     content:
       "I’m happy to share that I’m starting a new position as Senior Software Engineer at Google!",
     contentLink: "",
-    mediaURL: "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686811863/tech-social/shutterstock_630500720-4_bwekkt.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686811863/tech-social/shutterstock_630500720-4_bwekkt.jpg",
     likes: {
       likeCount: 23,
       likedBy: [],
@@ -50,9 +52,9 @@ export const posts = [
         text: "Congratulations John!",
         votes: {
           upvotedBy: [],
-          downvotedBy: []
-        }
-      }
+          downvotedBy: [],
+        },
+      },
     ],
     username: "johnwilliam",
     createdAt: "2022-03-12T10:35:21+05:30",
@@ -62,8 +64,10 @@ export const posts = [
     _id: "gs5eubh_7hrk_6dg5_GfguQB",
     content:
       "I write my first technical blog about querySelector() and querySelectorAll() on elements in JavaScript.",
-    contentLink: "https://sudipta26.hashnode.dev/introducing-queryselector-and-queryselectorall-on-elements-in-javascript",
-    mediaURL: "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686808219/tech-social/EDAef5fL7_zkli5s.jpg",
+    contentLink:
+      "https://sudipta26.hashnode.dev/introducing-queryselector-and-queryselectorall-on-elements-in-javascript",
+    mediaURL:
+      "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686808219/tech-social/EDAef5fL7_zkli5s.jpg",
     likes: {
       likeCount: 6,
       likedBy: [],
@@ -79,7 +83,8 @@ export const posts = [
     content:
       "Optimizing Android app performance: Minimize APK size and optimize resource usage for faster app installation and startup.",
     contentLink: "",
-    mediaURL: "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686814593/tech-social/How_to_Become_an_Android_Developer_qe0mpv.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686814593/tech-social/How_to_Become_an_Android_Developer_qe0mpv.jpg",
     likes: {
       likeCount: 12,
       likedBy: [],
@@ -91,11 +96,12 @@ export const posts = [
     updatedAt: formatDate(),
   },
   {
-    _id: "sfgjuk_ftbj_6789_fghuSD",
+    _id: "gukRj5k_f5bj_6s89_fg5uSy",
     content:
       "Complete Backend Developer Roadmap in 2023. 150+ Free + Paid Resources. Drop a 👋 in the comments. Make sure you follow me so that I can DM You.",
     contentLink: "",
-    mediaURL: "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686843689/tech-social/FyZwDsoXgAIi2H-_ydbpe4.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686843689/tech-social/FyZwDsoXgAIi2H-_ydbpe4.jpg",
     likes: {
       likeCount: 17,
       likedBy: [],
@@ -108,9 +114,9 @@ export const posts = [
         text: "👋",
         votes: {
           upvotedBy: [],
-          downvotedBy: []
-        }
-      }
+          downvotedBy: [],
+        },
+      },
     ],
     username: "linaabott",
     createdAt: "2023-06-12T10:25:07+05:30",
@@ -121,7 +127,8 @@ export const posts = [
     content:
       "🚀Join us at agprop and help shape the future of real estate! We're hiring talented interns and developers for Full Stack, Frontend, Backend, UI/UX, & DevOps roles. Work with cutting-edge tech like cloud and machine learning. Send your resume to vrijraj.s@agprop.in",
     contentLink: "",
-    mediaURL: "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686846308/tech-social/FvRFiZ9XoAA3xxo_n0rclt.jpg",
+    mediaURL:
+      "https://res.cloudinary.com/dqlasoiaw/image/upload/v1686846308/tech-social/FvRFiZ9XoAA3xxo_n0rclt.jpg",
     likes: {
       likeCount: 15,
       likedBy: [],
@@ -131,5 +138,5 @@ export const posts = [
     username: "julieadams",
     createdAt: "2022-04-22T10:25:24+05:30",
     updatedAt: formatDate(),
-  }
+  },
 ];
