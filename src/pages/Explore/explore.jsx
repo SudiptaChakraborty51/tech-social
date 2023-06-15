69d8a5f1877285ec72d22d364abaca9c1e0f70ad
Sudipta <hisudipta26@gmail.com>
2023-06-15 19:48:01 +0530

    refactor: explore style

--- src/pages/Explore/explore.jsx
+++ src/pages/Explore/explore.jsx
@@ -4,16 +4,30 @@ import Navbar from "../../components/Navbar/navbar";
 import LeftSideBar from "../../components/LeftSideBar/leftSideBar";
 import RightSideBar from "../../components/RightSideBar/rightSideBar";
 import { AuthContext } from "../../contexts/authContext";
+import { DataContext } from "../../contexts/dataContext";
+import PostCard from "../../components/PostCard/postCard";
 
 const Explore = () => {
   document.title = "tech-social | Explore";
   const { authState } = useContext(AuthContext);
+  const { dataState, postsLoading } = useContext(DataContext);
+
   return (
     <div className="explore">
       <Navbar />
       <div className="explore-content">
         <LeftSideBar />
-        <div className="explore-main">Explore</div>
+        <div className="explore-main">
+          {postsLoading ? (
+            <p>Loading...</p>
+          ) : (
+            dataState?.posts?.map((post) => (
+              <div key={post?._id}>
+                <PostCard post={post} />
+              </div>
+            ))
+          )}
+        </div>
         {authState?.token && <RightSideBar />}
       </div>
     </div>
