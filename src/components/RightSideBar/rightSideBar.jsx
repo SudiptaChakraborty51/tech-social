f5c3ea3f6ccec8ed7a14c1036a54fc84a8620cf8
Sudipta <hisudipta26@gmail.com>
2023-06-20 22:04:20 +0530

    feat: search users

--- src/components/RightSideBar/rightSideBar.jsx
+++ src/components/RightSideBar/rightSideBar.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./rightSideBar.css";
 import SuggestedUser from "../SuggestedUser/suggestedUser";
+import SearchBar from "../SearchBar/searchBar";
 
 const RightSideBar = () => {
   return (
     <div className="right-sidebar">
-      <div className="search-bar">
-        <input type="text" placeholder="Search Users" />
-        <i class="fa-solid fa-magnifying-glass"></i>
-      </div>
+      <SearchBar />
       <div className="suggested-users">
         <h4>Suggestions for you</h4>
         <SuggestedUser />
---
+++ src/components/SearchBar/searchBar.jsx
@@ -0,0 +1,91 @@
+import React, { useContext, useState } from "react";
+import "./searchBar.css";
+import { DataContext } from "../../contexts/dataContext";
+import { useNavigate } from "react-router-dom";
+
+export const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchedUsers, setSearchedUsers] = useState([]);
+
+  const { dataState } = useContext(DataContext);
+
+  const navigate = useNavigate();
+
+  const inputChangeHandler = (e) => {
+    setSearchInput(e.target.value);
+    const filteredResults = dataState?.users?.filter(
+      (user) =>
+        user.firstName.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        user.lastName.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        user.username.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setSearchedUsers(filteredResults);
+  };
+
+  const clearSearch = () => {
+    setSearchInput("");
+    setSearchedUsers([]);
+  };
+
+  return (
+    <div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search Users"
+          value={searchInput}
+          onChange={inputChangeHandler}
+        />
+        {searchInput.trim().length === 0 ? (
+          <i class="fa-solid fa-magnifying-glass"></i>
+        ) : (
+          <i class="fa-solid fa-xmark" onClick={clearSearch}></i>
+        )}
+      </div>
+      <div className="search-main-container">
+      {searchedUsers.length > 0 && searchInput.trim().length > 0 ? (
+        <div className="searched-users-container">
+          {searchedUsers?.map(
+            ({ _id, firstName, lastName, username, profileAvatar }) => {
+              return (
+                <li key={_id} className="searched-user">
+                  <div
+                    className="searched-user-name-profile"
+                    onClick={() => {
+                      navigate(`/profile/${username}`);
+                      clearSearch();
+                    }}
+                  >
+                    <img
+                      className="user-avatar"
+                      src={
+                        profileAvatar ||
+                        `https://res.cloudinary.com/dqlasoiaw/image/upload/v1686688962/tech-social/blank-profile-picture-973460_1280_d1qnjd.png`
+                      }
+                      alt="avatar"
+                    />
+                    <div className="searchedUser-name">
+                      <span>
+                        {firstName} {lastName}
+                      </span>
+                      <small>@{username}</small>
+                    </div>
+                  </div>
+                </li>
+              );
+            }
+          )}
+        </div>
+      ) : (
+        searchInput.trim().length > 0 && (
+          <div className="searched-users-container">
+            <p>User not found!</p>
+          </div>
+        )
+      )}
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
