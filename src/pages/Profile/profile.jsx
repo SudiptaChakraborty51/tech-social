1ce9571a7d4d30411cb018fd53e55419c7981e86
Sudipta <hisudipta26@gmail.com>
2023-07-02 12:35:34 +0530

    refactored profile container with bg image

--- src/pages/Profile/profile.jsx
+++ src/pages/Profile/profile.jsx
@@ -35,11 +35,15 @@ const Profile = () => {
     type: "",
   });
 
+  const [usersLoading, setUsersLoading] = useState(false);
+
   const getUserDetails = async () => {
     try {
+      setUsersLoading(true);
       const { data, status } = await axios.get(`/api/users/${username}`);
       if (status === 200) {
         setProfileData(data?.user);
+        setUsersLoading(false);
       }
     } catch (e) {
       console.error(e);
@@ -69,16 +73,18 @@ const Profile = () => {
       <div className="profile-content">
         <LeftSideBar />
         <div className="profile-main">
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100%"
-            }}
-          >
-            {Object.keys(profileData)?.length > 0 ? (
+          {usersLoading ? (
+            <ClipLoader color="var(--primary-dark)" size={50} />
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
               <div
                 className={`profile-container ${
                   darkMode && "bgSecondaryDarkMode"
@@ -103,82 +109,100 @@ const Profile = () => {
                   />
                 )}
                 <div className="profile-container-header">
-                  <div className="profile-name-avatar">
-                    <img
-                      className="avatar"
-                      src={
-                        profileData?.profileAvatar ||
-                        `https://res.cloudinary.com/dqlasoiaw/image/upload/v1686688962/tech-social/blank-profile-picture-973460_1280_d1qnjd.png`
-                      }
-                      alt="avatar"
-                    />
-                    <div>
-                      <h3>
-                        {profileData?.firstName} {profileData?.lastName}
-                      </h3>
-                      <small>@{profileData?.username}</small>
+                  <img
+                    className="background-img"
+                    src={
+                      profileData?.backgroundImage ||
+                      `https://res.cloudinary.com/dqlasoiaw/image/upload/v1688279047/tech-social/programming-code-coding-or-hacker-background-programming-code-icon-made-with-binary-code-digital-binary-data-and-streaming-digital-code-matrix-background-with-digits-1-0-illustration-vector_vzvd5n.jpg`
+                    }
+                    alt="background-pic"
+                  />
+                  <div className="profile-container-content">
+                    <div className="avatar-edit-follow-button">
+                      <span>
+                        <img
+                          className="avatar"
+                          src={
+                            profileData?.profileAvatar ||
+                            `https://res.cloudinary.com/dqlasoiaw/image/upload/v1686688962/tech-social/blank-profile-picture-973460_1280_d1qnjd.png`
+                          }
+                          alt="avatar"
+                        />
+                      </span>
+                      <div className="edit-follow-button">
+                        {profileData?.username === authState?.user?.username ? (
+                          <button
+                            className="edit-button"
+                            onClick={() => setEditProfileModal(true)}
+                          >
+                            Edit Profile
+                          </button>
+                        ) : (
+                          <button
+                            className={
+                              isFollowed(dataState?.users, profileData?._id)
+                                ? "following-button"
+                                : "follow-button"
+                            }
+                            onClick={() => {
+                              if (authState?.token) {
+                                if (
+                                  isFollowed(dataState?.users, profileData?._id)
+                                ) {
+                                  unfollowUserHandler(
+                                    authState?.token,
+                                    profileData?._id,
+                                    dataDispatch
+                                  );
+                                } else {
+                                  followUserHandler(
+                                    authState?.token,
+                                    profileData?._id,
+                                    dataDispatch
+                                  );
+                                }
+                              } else {
+                                toast.error("Please login to follow");
+                                navigate("/login");
+                              }
+                            }}
+                          >
+                            {isFollowed(dataState?.users, profileData?._id)
+                              ? "Following"
+                              : "Follow"}
+                          </button>
+                        )}
+                      </div>
                     </div>
                   </div>
-                  {profileData?.username === authState?.user?.username ? (
-                    <button className="edit-button">
-                      <i
-                        className="fa-solid fa-pen fa-md"
-                        onClick={() => setEditProfileModal(true)}
-                      ></i>
-                    </button>
-                  ) : (
-                    <button
-                      className={
-                        isFollowed(dataState?.users, profileData?._id)
-                          ? "following-button"
-                          : "follow-button"
-                      }
-                      onClick={() => {
-                        if (authState?.token) {
-                          if (isFollowed(dataState?.users, profileData?._id)) {
-                            unfollowUserHandler(
-                              authState?.token,
-                              profileData?._id,
-                              dataDispatch
-                            );
-                          } else {
-                            followUserHandler(
-                              authState?.token,
-                              profileData?._id,
-                              dataDispatch
-                            );
-                          }
-                        } else {
-                          toast.error("Please login to follow");
-                          navigate("/login");
-                        }
-                      }}
-                    >
-                      {isFollowed(dataState?.users, profileData?._id)
-                        ? "Following"
-                        : "Follow"}
-                    </button>
-                  )}
                 </div>
-                {profileData?.bio && <p>{profileData?.bio}</p>}
-                {profileData?.website && (
-                  <a
-                    style={{ wordBreak: "break-all" }}
-                    href={profileData?.website}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {profileData?.website}
-                  </a>
-                )}
-                <p>
-                  <i className="fa-solid fa-calendar"></i> Joined{" "}
-                  {` ${new Date(profileData?.createdAt)
-                    .toDateString()
-                    .split(" ")
-                    .slice(1, 4)
-                    .join(" ")}`}
-                </p>
+                <div className="profile-details">
+                  <div className="profile-name-username">
+                    <h3>
+                      {profileData?.firstName} {profileData?.lastName}
+                    </h3>
+                    <small>@{profileData?.username}</small>
+                  </div>
+                  {profileData?.bio && <p>{profileData?.bio}</p>}
+                  {profileData?.website && (
+                    <a
+                      style={{ wordBreak: "break-all" }}
+                      href={profileData?.website}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {profileData?.website}
+                    </a>
+                  )}
+                  <p>
+                    <i className="fa-solid fa-calendar"></i> Joined{" "}
+                    {` ${new Date(profileData?.createdAt)
+                      .toDateString()
+                      .split(" ")
+                      .slice(1, 4)
+                      .join(" ")}`}
+                  </p>
+                </div>
                 <div className="profile-post-follow-details">
                   <p>
                     {userPosts?.length}{" "}
@@ -218,24 +242,23 @@ const Profile = () => {
                   </p>
                 </div>
               </div>
-            ) : (
-              <ClipLoader color="var(--primary-dark)" size={50} />
-            )}
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%"
-              }}
-            >
-              {userPosts.length > 0 &&
-                getSortedPosts(userPosts, "Latest")?.map((post) => (
-                  <PostCard key={post._id} post={post} />
-                ))}
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+              >
+                {userPosts.length > 0 &&
+                  getSortedPosts(userPosts, "Latest")?.map((post) => (
+                    <PostCard key={post._id} post={post} />
+                  ))}
+              </div>
             </div>
-          </div>
+          )}
         </div>
         <div className="rightSideBar">
           <RightSideBar />
